12ec371ccbf3edf1711fa7ad941adbf0e8d5b9d4
CurryPseudo <currypseudo@gmail.com>
2020-07-07 21:15:58 +0800

    Use lua file to test

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,17 @@ mod lexer;
 pub use lexer::*;
 
 fn main() {
+    use std::fs::File;
+    use std::io::prelude::*;
     let _ = pretty_env_logger::init();
     let mut vm = VM::new();
     vm.import_builtin_function();
-    add_code("
-local a = 5
-local b = a
-local c = 3
-print(1,a,b,555)
-print(c)
- print(5)", &mut vm);
+    let args: Vec<_> = std::env::args().collect();
+    let lua_file_name = &args[1];
+    let mut lua_file = File::open(lua_file_name).unwrap();
+    let mut lua_content = String::new();
+    lua_file.read_to_string(&mut lua_content).unwrap();
+    add_code(&lua_content, &mut vm);
     vm.instructions.push(Instruction::Return(0, 1));
     while vm.run() {}
 }
---
+++ test.lua
@@ -0,0 +1,5 @@
+local a = 3
+local b = a
+local c = 5
+print(a, b, c)
+print(b)
