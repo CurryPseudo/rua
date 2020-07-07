efb0ad06b63cc64f8e9f70af89a9e51ec3473c85
CurryPseudo <currypseudo@gmail.com>
2020-07-07 19:32:56 +0800

    Refactor FunctionStack API

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,9 @@ fn main() {
     let _ = pretty_env_logger::init();
     let mut vm = VM::new();
     vm.import_builtin_function();
-    let stack = FunctionStack::from_statements(
-"print(1,2,4,555)print(5)");
-    vm.add_function(stack);
+    add_code("print(1,2,4,555)
+print(333)
+ print(5)", &mut vm);
     vm.instructions.push(Instruction::Return(0, 1));
     while vm.run() {}
 }
--- src/parser.rs
+++ src/parser.rs
@@ -4,8 +4,8 @@ use std::collections::HashMap;
 use std::collections::VecDeque;
 #[macro_use]
 mod lalr1;
-use lalr1::*;
 use crate::lexer::*;
+use lalr1::*;
 macro_rules! pop_front_unwrap {
     ($vec_deque:ident, $into_inner:tt) => {
         $vec_deque.pop_front().unwrap().$into_inner().unwrap()
@@ -13,7 +13,7 @@ macro_rules! pop_front_unwrap {
 }
 #[derive(Debug, EnumAsInner)]
 enum AST {
-    Function { name: String, param_list: Vec<i32> },
+    Function { name: String, param_list: Box<AST> },
     ParamList(Vec<i32>),
     Statements(VecDeque<AST>),
 }
@@ -34,8 +34,7 @@ parser!(
     };
     Function -> id, left_bracket, ParamList, right_bracket => {
         let name = pop_front_unwrap!(token, into_id);
-        let param_list = pop_front_unwrap!(ast, into_param_list);
-        AST::Function{ name, param_list }
+        AST::Function{ name, param_list: Box::new(ast.pop_front().unwrap()) }
     };
     ParamList -> ParamList, comma, number => {
         let mut param_list = pop_front_unwrap!(ast, into_param_list);
@@ -88,25 +87,39 @@ impl FunctionStack {
     fn get_constant_rk_index(&mut self, value: Value) -> i32 {
         -1 - self.get_constant_index(value) as i32
     }
-    fn add_statement(&mut self, ast: AST) {
-        let (name, param_list) = ast.into_function().unwrap();
-        let name = Value::String(String::from(name));
-        let param_len = param_list.len();
-        let r = self.get_free_variable_index_temp(param_len as u32 + 1);
-        let k0 = self.get_constant_rk_index(name) as i32;
-        self.instructions.push(Instruction::GetTabUp(r, 0, k0));
-        for (i, param) in param_list.into_iter().enumerate() {
-            let k1 = self.get_constant_index(Value::Number(param));
-            self.instructions.push(Instruction::LoadK(r + 1 + i as u32, k1));
+    fn add(&mut self, ast: AST) {
+        use AST::*;
+        match ast {
+            Function { name, param_list } => {
+                let name = Value::String(String::from(name));
+                let param_list = param_list.into_param_list().unwrap();
+                let param_len = param_list.len();
+                let r = self.get_free_variable_index_temp(param_len as u32 + 1);
+                let k0 = self.get_constant_rk_index(name) as i32;
+                self.instructions.push(Instruction::GetTabUp(r, 0, k0));
+                for (i, param) in param_list.into_iter().enumerate() {
+                    let k1 = self.get_constant_index(Value::Number(param));
+                    self
+                        .instructions
+                        .push(Instruction::LoadK(r + 1 + i as u32, k1));
+                }
+                self
+                    .instructions
+                    .push(Instruction::Call(r, param_len as u32 + 1, 1));
+            }
+            Statements(functions) => {
+                for function in functions {
+                    self.add(function);
+                }
+            }
+            _ => panic!()
         }
-        self.instructions.push(Instruction::Call(r, param_len as u32 + 1, 1));
     }
-    pub fn from_statements(input: &str) -> Self {
-        let functions = lua_parse(Token::lexer(input).into_iter().collect()).into_statements().unwrap();
-        let mut r = Self::default();
-        for function in functions {
-            r.add_statement(function);
-        }
-        r
-    }
+}
+pub fn add_code(input: &str, vm: &mut VM) {
+    let mut stack = FunctionStack::default();
+    let tokens = Token::lexer(input).collect();
+    let ast = lua_parse(tokens);
+    stack.add(ast);
+    vm.add_function(stack);
 }
